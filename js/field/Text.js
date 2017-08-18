23c14ca8149dc065008ec67031f2e6a11788993b
lrodriguezcabrales <lizethcabrales@gmail.com>
2017-08-17 20:26:29 -0500

    views

---
+++ js/field/Text.js
@@ -0,0 +1,17 @@
+class Text{
+
+	constructor() {
+
+	}
+
+	init (argument) {
+		var text = $('<input type="text"></input');
+
+		return text;
+	}
+
+	value (argument) {
+		console.log('value jajaja');
+	}
+
+}
